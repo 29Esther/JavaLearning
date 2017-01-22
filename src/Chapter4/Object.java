bc4c19436444d93d44b3626e036bf011c6abb66b
29Esther <yaojingshuihudie@163.com>
2017-01-22 09:48:20 +0800

    Init Chapter4

---
+++ src/Chapter4/Object.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chapter4;
+
+/**
+ * 类是对象的设计图，对象是类的实现
+ * 一个.java文件中可以有多个class，但只能有一个public的，且名称与文件名相同
+ * @author lzx
+ */
+public class Object {
+    
+}
